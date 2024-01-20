e755804f0a5c4dcf8e4ec58bb240bc9a3f83348d
Alan <tyujn24@mail.ru>
2024-01-20 03:34:48 +0300

    shit

--- src/view/pages/Landing/index.tsx
+++ src/view/pages/Landing/index.tsx
@@ -1,30 +1,26 @@
 import React from 'react';
 import classNames from 'classnames';
 import styles from './index.module.scss';
-import { useGetProductsQuery } from '../../../core/api/product';
 import { FactoryImage } from '../../../assets/image/FactoryImage';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 import { About } from '../../components/About';
+import { CatalogPart } from '../../components/CatalogPart';
 import { Benefits } from '../../components/Benefits/index.tsx';
 
 const cx = classNames.bind(styles);
 
 export const Landing: React.FC = () => {
     const { data } = useGetProductsQuery();
-    const titles = ['a', 'b', 'c', 'd'];
-    const descriptions = ['a', 'b', 'c', 'd'];
 
     console.log(data);
     return (
         <div className={cx(styles.landing)}>
-            <ContentWrapper>
-                <Header />
-                <FactoryImage />
-                <Benefits titles={titles} descriptions={descriptions} />
-                <About />
-                <Footer />
-            </ContentWrapper>
+            <Header />
+            <FactoryImage />
+            <About />
+            <CatalogPart />
+            <Footer />
         </div>
     );
 };
