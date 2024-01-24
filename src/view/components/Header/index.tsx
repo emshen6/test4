dfae16f2b1a62337b5cd599bfafdb49ca6db7bf2
dustov2003 <dustov2003@gmail.com>
2024-01-24 19:44:45 +0300

    shit x2 searching

--- src/view/components/Header/index.tsx
+++ src/view/components/Header/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
-import { ru } from './i18n/ru.ts';
 import styles from './index.module.scss';
+import { Search } from './Search';
 import { ICON } from '../UI/Icon/IconType.tsx';
 import Icon from '../UI/Icon';
 import { ContentWrapper } from '../ContentWrapper';
@@ -14,7 +14,7 @@ export const Header: React.FC = () => {
         <ContentWrapper>
             <div className="flex justify-between py-6">
                 <Icon icon={ICON.LOGO} width={175} height={55} color={'black'} />
-                <div className="m-5 flex gap-10">
+                <div className="grid grid-flow-col items-center gap-10">
                     <input type={'text'} placeholder={ru.searchPlaceholder} className={cx(styles.input)} />
                     <div className="grid grid-flow-col gap-5">
                         <Link to={'/catalog'}>
